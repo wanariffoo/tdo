609042617099ca3cd98fca0acd94339216a19eef
Wan Arif <wan.wanabhar@ruhr-uni-bochum.de>
2019-12-12 00:39:48 +0100

    .

---
+++ include/mycudaheader.h
@@ -0,0 +1,354 @@
+
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#define CUDA_CALL( call )                                                                                          \
+    {                                                                                                                  \
+    cudaError_t err = call;                                                                                          \
+    if ( cudaSuccess != err)                                                                                         \
+        fprintf(stderr, "CUDA error for %s in %d of %s : %s.\n", #call , __LINE__ , __FILE__ ,cudaGetErrorString(err));\
+    }
+
+#include <cmath>
+#include <ctime>
+#include <iostream>
+
+
+// Self-defined double-precision atomicAdd function for nvidia GPUs with Compute Capability 6 and below.
+// Pre-defined atomicAdd() with double-precision does not work for pre-CC7 nvidia GPUs.
+__device__ 
+double atomicAdd_double(double* address, double val)
+{
+    unsigned long long int* address_as_ull =
+                                (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        old = atomicCAS(address_as_ull, assumed,
+                        __double_as_longlong(val +
+                                __longlong_as_double(assumed)));
+
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+
+// Determines 1-dimensional CUDA block and grid sizes based on the number of rows N
+__host__ 
+void calculateDimensions(size_t N, dim3 &blockDim, dim3 &gridDim)
+{
+    if ( N <= 1024 )
+    {
+        blockDim.x = N; blockDim.y = 1; blockDim.z = 1;
+        gridDim.x  = 1; gridDim.y = 1; gridDim.z = 1;
+    }
+        
+    else
+    {
+        blockDim.x = 1024; blockDim.y = 1; blockDim.z = 1;
+        gridDim.x  = (int)ceil(N/blockDim.x)+1; gridDim.y = 1; gridDim.z = 1;
+    }
+}
+
+__global__
+void setToZero(double* a, size_t num_rows)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if ( id < num_rows )
+		a[id] = 0.0;
+}
+
+// norm = x.norm()
+__global__ 
+void norm_GPU(double* norm, double* x, size_t num_rows)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	// TODO: if (id < num)
+
+	if ( id == 0 )
+		*norm = 0;
+	__syncthreads();
+
+	if ( id < num_rows )
+	{
+		atomicAdd_double( norm, x[id]*x[id] );
+	}
+	__syncthreads();
+
+	if ( id == 0 )
+		*norm = sqrt(*norm);
+}
+
+// a[] = 0
+
+
+// a[] = 0, size_t
+__global__
+void setToZero(size_t* a, size_t num_rows)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if ( id < num_rows )
+		a[id] = 0.0;
+}
+
+//TODO: to delete
+// bool = true
+__global__
+void setToTrue( bool *foo )
+{
+	*foo = true;
+}
+
+
+// DEBUG: TEST !!!!!!!!!!!!!!!!!!!!!!!!!!
+__global__
+void sqrt_GPU(double *x)
+{
+	*x = sqrt(*x);
+}
+
+// sum = sum( x[n]*x[n] )
+__global__ 
+void sumOfSquare_GPU(double* sum, double* x, size_t n)
+{
+	int id = threadIdx.x + blockDim.x*blockIdx.x;
+	int stride = blockDim.x*gridDim.x;
+		
+	__shared__ double cache[1024];
+	
+	double temp = 0.0;
+	while(id < n)
+	{
+		temp += x[id]*x[id];
+		
+		id += stride;
+	}
+	
+	cache[threadIdx.x] = temp;
+	
+	__syncthreads();
+	
+	// reduction
+	unsigned int i = blockDim.x/2;
+	while(i != 0){
+		if(threadIdx.x < i){
+			cache[threadIdx.x] += cache[threadIdx.x + i];
+		}
+		__syncthreads();
+		i /= 2;
+	}
+
+	// reset id
+	id = threadIdx.x + blockDim.x*blockIdx.x;
+
+	// reduce sum from all blocks' cache
+	if(threadIdx.x == 0)
+		atomicAdd_double(sum, cache[0]);
+}
+
+
+__global__ 
+void LastBlockSumOfSquare_GPU(double* sum, double* x, size_t n, size_t counter)
+{
+	int id = threadIdx.x + blockDim.x*blockIdx.x;
+    
+    // if ( id >= counter*blockDim.x && id < ( ( counter*blockDim.x ) + lastBlockSize ) )
+    if ( id >= counter*blockDim.x && id < n )
+		atomicAdd_double(sum, x[id]*x[id]);
+}
+
+__host__
+void norm_GPU(double* d_norm, double* d_x, size_t N, dim3 gridDim, dim3 blockDim)
+{
+	setToZero<<<1,1>>>( d_norm, 1);
+    
+    // getting the last block's size
+    size_t lastBlockSize = N;
+    size_t counter = 0;
+
+    if ( N % gridDim.x == 0 ) {}
+       
+
+    else
+    {
+        while ( lastBlockSize >= gridDim.x)
+        {
+            counter++;
+            lastBlockSize -= gridDim.x;
+        }
+    }
+
+    // sum of squares for the full blocks
+    // sumOfSquare_GPU<<<gridDim.x - 1, blockDim>>>(d_norm, d_x, N); // TODO: check, this is the original
+    sumOfSquare_GPU<<<gridDim.x - 1, blockDim>>>(d_norm, d_x, (gridDim.x - 1)*blockDim.x);
+
+    // sum of squares for the last incomplete block
+    LastBlockSumOfSquare_GPU<<<1, lastBlockSize>>>(d_norm, d_x, N, counter);
+	// cudaDeviceSynchronize();
+	sqrt_GPU<<<1,1>>>( d_norm );
+	// cudaDeviceSynchronize();
+}
+
+
+/// Helper functions for debugging
+__global__ 
+void print_GPU(double* x)
+{
+	printf("[GPU] x = %e\n", *x);
+}
+
+__global__ 
+void print_GPU(int* x)
+{
+	printf("[GPU] x = %d\n", *x);
+}
+
+__global__ 
+void print_GPU(size_t* x)
+{
+	printf("[GPU] x = %lu\n", *x);
+}
+
+__global__ 
+void print_GPU(bool* x)
+{
+	printf("[GPU] x = %d\n", *x);
+}
+
+__global__ 
+void printVector_GPU(double* x)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	printf("[GPU] x[%d] = %e\n", id, x[id]);
+}
+
+__global__
+void printVector_GPU(double* x, size_t num_rows)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	if ( id < num_rows )
+		printf("%d %e\n", id, x[id]);
+}
+
+__global__ 
+void printVector_GPU(std::size_t* x, size_t num_rows)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+	
+	if ( id < num_rows )
+		printf("%d %lu\n", id, x[id]);
+	
+}
+
+__global__ 
+void printVector_GPU(int* x)
+{
+	int id = blockDim.x * blockIdx.x + threadIdx.x;
+
+	printf("[GPU] x[%d] = %d\n", id, x[id]);
+}
+
+// (scalar) a = b
+__global__ 
+void equals_GPU(double* a, double* b)
+{
+	*a = *b;
+}
+
+
+// x = a * b
+__global__ 
+void dotProduct(double* x, double* a, double* b, size_t num_rows)
+{
+	unsigned int id = threadIdx.x + blockDim.x*blockIdx.x;
+	unsigned int stride = blockDim.x*gridDim.x;
+
+	__shared__ double cache[1024];
+
+	double temp = 0.0;
+
+	// filling in the shared variable
+	while(id < num_rows){
+		temp += a[id]*b[id];
+
+		id += stride;
+	}
+
+	cache[threadIdx.x] = temp;
+
+	__syncthreads();
+
+	// reduction
+	unsigned int i = blockDim.x/2;
+	while(i != 0){
+		if(threadIdx.x < i){
+			cache[threadIdx.x] += cache[threadIdx.x + i];
+		}
+		__syncthreads();
+		i /= 2;
+	}
+
+	if(threadIdx.x == 0){
+		atomicAdd_double(x, cache[0]);
+	}
+	__syncthreads();
+}
+
+
+__global__
+void LastBlockDotProduct(double* dot, double* x, double* y, size_t starting_index)
+{
+	int id = threadIdx.x + blockDim.x*blockIdx.x + starting_index;
+		
+	atomicAdd_double(dot, x[id]*y[id]);
+	
+}
+
+
+// dot = a[] * b[]
+__host__
+void dotProduct_test(double* dot, double* a, double* b, size_t N, dim3 gridDim, dim3 blockDim)
+{
+    setToZero<<<1,1>>>( dot, 1 );
+
+    // getting the last block's size
+    size_t lastBlockSize = blockDim.x - ( (gridDim.x * blockDim.x ) - N );
+
+	if ( N < blockDim.x)
+	{
+		LastBlockDotProduct<<<1, N>>>( dot, a, b, 0 );
+	}
+
+	else
+	{
+		// dot products for the full blocks
+		dotProduct<<<gridDim.x - 1, blockDim>>>(dot, a, b, (gridDim.x - 1)*blockDim.x );
+		
+		// dot products for the last incomplete block
+		LastBlockDotProduct<<<1, lastBlockSize>>>(dot, a, b, ( (gridDim.x - 1) * blockDim.x ) );
+	}
+
+}
+
+// x = y / z
+__global__
+void divide_GPU(double *x, double *y, double *z)
+{
+	*x = *y / *z;
+}
+ 
+
+
+
+
+
+#endif // MYHEADER_H
