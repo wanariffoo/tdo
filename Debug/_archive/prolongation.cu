87417e453fdd15632315602cc0f2b94374546a45
Wan Arif <wan.wanabhar@ruhr-uni-bochum.de>
2020-01-10 00:18:40 +0100

    prolong: added 1 and 0.5. Left 0.25 now

--- Debug/_archive/prolongation.cu
+++ Debug/_archive/prolongation.cu
@@ -13,10 +13,17 @@ int main()
 
     vector<vector<size_t>> N;
     N.resize(2, vector<size_t>(2));
-    N[0] = {3, 1};
-    N[1] = {6, 2};
+    N[0] = {1, 1};
+    N[1] = {2, 2};
 
-    vector<size_t> num_rows = { 16, 42 };
+    vector<size_t> num_rows = { 8, 18 };
+
+    size_t lev = 0;
+    size_t dim = 2;
+    vector<size_t> numNodes(2);
+
+    numNodes[0] = 4;
+    numNodes[1] = 9;
 
 
     vector<vector<vector<double>>> A;
@@ -25,19 +32,12 @@ int main()
     A[1].resize(num_rows[1], vector<double>(num_rows[1]));
 
     size_t numLevels = 2;
-    vector<vector<vector<size_t>>> P;
+    vector<vector<vector<double>>> P;
 
     P.resize(numLevels - 1);
-    P[0].resize(num_rows[1], vector<size_t>(num_rows[0]));
+    P[0].resize(num_rows[1], vector<double>(num_rows[0]));
 
 
-
-    size_t lev = 0;
-    size_t dim = 2;
-    vector<size_t> numNodes(2);
-
-    numNodes[0] = 8;
-    numNodes[1] = 21;
 
     // for (  int j = 0; j < 12 ; j++ )
     // {
@@ -61,12 +61,30 @@ int main()
     // P[lev][( 2*(j % ( (N[lev][0] + 1)*dim) )) + ( (j/(2*(N[lev][0] + 1))))*20+1][j+1] = 1;
 
 
-    // for ( int i = 0 ; i < numNodes[0]*2 ; i += 2)
-    for ( int i = 0 ; i < numNodes[0] * 2 ; i += 2)
+    // for ( int i = 0 ; i < 2 ; i += 2)
+    for ( int i = 0 ; i < numNodes[0]*2 ; i += 2)
     {
         for ( int j = 0 ; j < dim ; j++ )
         {
-            P[lev][( 2*(i % ( (N[lev][0] + 1)*dim) )) + ( (i/(2*(N[lev][0] + 1))))*2*dim*(N[lev+1][0] + 1) + j][i+j] = 1;
+            P[lev][( 2*(i % ( (N[lev][0] + 1)*dim) )) + ( (ceil)( i / ( 2*(N[lev][0] + 1 ) ) ) )*2*dim*(N[lev+1][0] + 1) + j][i+j] = 1;
+
+        // // east node
+        if ( (i / 2 + 1) % (N[lev][0]+1) != 0 )
+            P[lev][( 2*(i % ( (N[lev][0] + 1)*dim) )) + ( (ceil)( i / ( 2*(N[lev][0] + 1 ) ) ) )*2*dim*(N[lev+1][0] + 1) + j + 2][i+j] += 0.5;
+
+        // // north node
+        if ( i / 2 + (N[lev][0] + 1) < (N[lev][0] + 1)*(N[lev][0] + 1))
+            P[lev][( 2*(i % ( (N[lev][0] + 1)*dim) )) + ( (ceil)( i / ( 2*(N[lev][0] + 1 ) ) ) )*2*dim*(N[lev+1][0] + 1) + j + 2*(N[lev+1][0] + 1)][i+j] += 0.5;
+
+        // west node
+        if ( (i / 2) % (N[lev][0]+1) != 0 )
+            P[lev][( 2*(i % ( (N[lev][0] + 1)*dim) )) + ( (ceil)( i / ( 2*(N[lev][0] + 1 ) ) ) )*2*dim*(N[lev+1][0] + 1) + j - 2][i+j] += 0.5;
+
+        // south node
+        if ( i / 2 >= N[lev][0] + 1)
+            P[lev][( 2*(i % ( (N[lev][0] + 1)*dim) )) + ( (ceil)( i / ( 2*(N[lev][0] + 1 ) ) ) )*2*dim*(N[lev+1][0] + 1) + j - 2*(N[lev+1][0] + 1)][i+j] += 0.5;
+
+
         }
     }
 
